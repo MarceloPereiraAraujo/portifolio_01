535b67388c6d57164a1aa2ebcf50ca3689c2d59b
Marcelo Pereira Araujo <marcelo.ti.hoje@gmail.com>
2023-05-03 18:03:54 -0300

    segunda alteração

---
+++ MediaNotas.js
@@ -0,0 +1,14 @@
+const MediaNotas = (n1,n2) => {
+    return (n1 + n2)/2;
+}
+if (MediaNotas >= 6) {
+    console.log (("Sua média foi ") + (MediaNotas(7,6)));
+    console.log ("Está Aprovado");
+}else {
+    console.log (("Sua média foi ") + (MediaNotas(7,6)));
+    console.log ("Está Aprovado");
+}
+console.log (MediaNotas(7,6));
+
+
+
